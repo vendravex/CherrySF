abe4e99458b2d6fd71a2ca4956577de73e2bef24
vendravex <vendravex@gmail.com>
2019-11-15 00:21:55 +0700

    Update CherrysMain.js

--- CherrysMain.js
+++ CherrysMain.js
@@ -22,10 +22,10 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/p/preview-landing-page.html"}
-if(x=="2"){xxx="/p/preview-landing-page.html"}
-if(x=="3"){xxx="/p/stylish-homes.html"}
-if(x=="4"){xxx="/p/preview-landing-page.html"}
-if(x=="5"){xxx="/p/stylish-homes.html"}a_to_ve[i].href="https://android-cherry.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="/p/stylish-homes.html"}
+if(x=="3"){xxx="/p/preview-landing-page.html"}
+if(x=="4"){xxx="/p/stylish-homes.html"}
+if(x=="5"){xxx="/p/preview-landing-page.html"}a_to_ve[i].href="https://android-cherry.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
